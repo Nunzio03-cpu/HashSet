8e321bbfb9e437872b1f16e6262ba6234fd8c1f6
Nunzio <nunziodebartolomeo03@gmail.com>
2025-01-10 10:48:03 +0100

    Esercizio HashSet2 corretto

--- src/HashSet2/Main.java
+++ src/HashSet2/Main.java
@@ -1,40 +1,39 @@
 package HashSet2;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<Integer, String> hashMap1 = new HashMap<>();
-        hashMap1.put(1, "Uno");
-        hashMap1.put(2, "Due");
-        hashMap1.put(3, "Tre");
+        HashSet<String> hashSet = creaHashSet();
+        String oggettoDaVerificare = "Elemento2";
 
-        HashMap<Integer, String> hashMap2 = new HashMap<>() {{
-            put(4, "Quattro");
-            put(5, "Cinque");
-            put(6, "Sei");
-        }};
-
-        Map<Integer, String> hashMap3 = Map.of(
-                7, "Sette",
-                8, "Otto",
-                9, "Nove"
-        );
-
-        System.out.println("Contenuto di hashMap1:");
-        stampaMappa(hashMap1);
-
-        System.out.println("Contenuto di hashMap2:");
-        stampaMappa(hashMap2);
-
-        System.out.println("Contenuto di hashMap3:");
-        stampaMappa(hashMap3);
-    }
-
-    public static <K, V> void stampaMappa(Map<K, V> map) {
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ", " + entry.getValue());
+        Iterator<String> iterator = hashSet.iterator();
+        while (iterator.hasNext()) {
+            String elemento = iterator.next();
+            if (elemento.equals(oggettoDaVerificare)) {
+                iterator.remove();
+                System.out.println("Rimosso: " + elemento);
+            }
         }
+
+        // Svuotare l'HashSet
+        hashSet.clear();
+        System.out.println("HashSet svuotato.");
+
+        // Verificare se l'HashSet è vuoto
+        if (hashSet.isEmpty()) {
+            System.out.println("L'HashSet è vuoto.");
+        } else {
+            System.out.println("L'HashSet contiene ancora elementi.");
+        }
+    }
+    public static HashSet<String> creaHashSet() {
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("Elemento1");
+        hashSet.add("Elemento2");
+        hashSet.add("Elemento3");
+        hashSet.add("Elemento4");
+        return hashSet;
     }
 }
