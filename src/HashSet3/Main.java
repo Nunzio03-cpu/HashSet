8d1f0aacab12a0ce1f9b838d9ef10e8ae01e6927
Nunzio <nunziodebartolomeo03@gmail.com>
2025-01-10 10:50:05 +0100

    Esercizio HashSet3

--- src/HashSet3/Main.java
+++ src/HashSet3/Main.java
@@ -1,4 +1,39 @@
 package HashSet3;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Main {
+
+    public static void main(String[] args) {
+        HashSet<String> hashSet = creaHashSet();
+        String oggettoDaVerificare = "Elemento2";
+
+        Iterator<String> iterator = hashSet.iterator();
+        while (iterator.hasNext()) {
+            String elemento = iterator.next();
+            if (elemento.equals(oggettoDaVerificare)) {
+                iterator.remove();
+                System.out.println("Rimosso: " + elemento);
+            }
+        }
+
+        hashSet.clear();
+        System.out.println("HashSet svuotato.");
+
+        if (hashSet.isEmpty()) {
+            System.out.println("L'HashSet è vuoto.");
+        } else {
+            System.out.println("L'HashSet contiene ancora elementi.");
+        }
+    }
+
+    public static HashSet<String> creaHashSet() {
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("Elemento1");
+        hashSet.add("Elemento2");
+        hashSet.add("Elemento3");
+        hashSet.add("Elemento4");
+        return hashSet;
+    }
 }
